a2a0ad2f53c59d6cef7595f3368f2c5874a2c78b
uriel621 <uriel621@live.com>
2018-03-16 17:19:34 -0700

    db creation

--- strong/admin.py
+++ strong/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from strong.models import Project, Images
+# Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Images)
--- strong/models.py
+++ strong/models.py
@@ -1,19 +1,32 @@
 from django.db import models
 
 # Create your models here.
-# class Project(models.Model):
-#     user = models.ForeignKey(User)
-#     title = models.CharField(max_length=128)
-#     body = models.CharField(max_length=400)
 
-# def get_image_filename(instance, filename):
-#     title = instance.post.title
-#     slug = slugify(title)
-#     return "project_images/%s-%s" % (slug, filename)  
+class Project(models.Model):
+    type_of_project = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
 
+    def __str__(self):
+        return self.type_of_project
 
+def get_image_filename(instance, filename):
+    print('----> ', instance.project)
+    print('INSTANCE----> ', instance.project)
+    print('FILENAME----> ', filename)
+    return "{}/{}".format(instance.project, filename)  
 
-# class Images(models.Model):
-#     post = models.ForeignKey(Post, default=None)
-#     image = models.ImageField(upload_to=get_image_filename,
-#                               verbose_name='Image', )
+def name_of_project(name):
+    new_name = ''
+    for letter in str(name):
+        if letter == '/':
+            break
+        else:
+            new_name += letter
+    return "{}".format(new_name) 
+
+class Images(models.Model):  
+    project = models.ForeignKey(Project, default=None)
+    image = models.ImageField(upload_to=get_image_filename)
+    def __str__(self):
+        return name_of_project(self.image)
+    
